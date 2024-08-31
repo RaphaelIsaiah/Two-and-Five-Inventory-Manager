b1f7a88a7f6b1238869a7df5b26ce7303f934f84
RaphaelIsaiah <rafvie12@gmail.com>
2024-08-31 08:43:19 +0100

    Solved the wrong display in the forms.py

--- two_and_five_inventory_manager/inventory/forms.py
+++ two_and_five_inventory_manager/inventory/forms.py
@@ -1,10 +1,11 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    usable_password = None # removes the password authentication display.
 
     class Meta:
         model = User
