4a0e4b6c97f91f91b954a87aae189f17da3a36ea
RaphaelIsaiah <rafvie12@gmail.com>
2024-08-31 15:54:40 +0100

    Functionality to add items done. Related links added.

--- two_and_five_inventory_manager/inventory/views.py
+++ two_and_five_inventory_manager/inventory/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView, View
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, View, CreateView
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .forms import UserRegisterForm
-from .models import InventoryItem
+from .forms import UserRegisterForm, InventoryItemForm
+from .models import InventoryItem, Category
 
 
 class Index(TemplateView):
@@ -40,3 +41,23 @@ class SignUpView(View):
 
         # if the form is not valid, it renders this to avoid errors.
         return render(request, "inventory/signup.html", {"form": form})
+
+
+class AddItem(LoginRequiredMixin, CreateView):
+    model = InventoryItem
+    form_class = InventoryItemForm
+    template_name = (
+        "inventory/item_form.html"  # html template that renders to the screen
+    )
+    success_url = reverse_lazy("dashboard")  # redirects to the dashboard page
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)  # gets the original context
+        context["categories"] = Category.objects.all()  # adds a value to it
+        return context  # returns the context dictionary after the change
+
+    # removes the risk of errror
+    # sets the user on the form by default to be the loggedin user
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
