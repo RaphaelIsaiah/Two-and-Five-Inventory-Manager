770dd73d55a016723a273ef4ab9748601f94d3ef
RaphaelIsaiah <rafvie12@gmail.com>
2024-08-31 14:09:36 +0100

    Created InventoryItem and Category models

--- two_and_five_inventory_manager/inventory/models.py
+++ two_and_five_inventory_manager/inventory/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class InventoryItem(models.Model):
+    name = models.CharField(max_length=200)
+    quantity = models.IntegerField()
+    category = models.ForeignKey(
+        "Category", on_delete=models.SET_NULL, blank=True, null=True
+    )
+    date_created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name  # this is what will display as the name of the model.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
